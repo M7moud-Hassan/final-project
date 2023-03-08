958da8ba254b3ef25f528603b6fdd72e2784775b
mahmoud hassan <abosamour990@gmail.com>
2023-03-08 18:40:38 +0200

    fix bug

--- api/authentication/models.py
+++ api/authentication/models.py
@@ -33,3 +33,30 @@ class Education (models.Model):
     description = models.CharField(max_length=500)
     id = models.AutoField
 
+class Skills(models.Model):
+    name=models.CharField(max_length=50)
+    id=models.AutoField
+
+class JobTitle(models.Model):
+    name=models.CharField(max_length=50)
+    id=models.AutoField
+class RegisterFreelancer(models.Model):
+    id = models.AutoField
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email=models.CharField(max_length=100)
+    is_active=models.BooleanField(default=False)
+    phone_number=models.CharField(max_length=11)
+    password=models.CharField(max_length=300)
+    job_title=models.ForeignKey(JobTitle,on_delete=models.CASCADE)
+    overview=models.CharField(max_length=500)
+    hourly_rate=models.DecimalField(null=True,decimal_places=2,max_digits=10),
+    user_image=models.ImageField(upload_to='static_dirs/images/user_image')
+    street_address=models.CharField(max_length=50)
+    city=models.CharField(max_length=50)
+    state=models.CharField(max_length=50)
+    postal_code=models.CharField(max_length=20)
+    experience=models.ManyToManyField(Experience)
+    education=models.ManyToManyField(Education)
+    skills=models.ManyToManyField(Skills)
+    services=models.ManyToManyField(Services)
