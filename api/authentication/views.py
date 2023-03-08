3c80653e8708f530826379480e23aeab92fb3bb2
Ahmed Abdelnasser <ahmed.abdelnasser7777777@gmail.com>
2023-03-09 00:26:03 +0200

    handle Forget password 'notice : will continue tomorrow isa'

--- api/authentication/views.py
+++ api/authentication/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_http_methods
+from pyexpat.errors import messages
+from .helpers import send_forget_password_mail
+from api.authentication.models import RegisterFreelancer
+
 
 # Create your views here.
+@require_http_methods(["POST"])
+def emailResetPassword(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        if not RegisterFreelancer.objects.filter(email=email).first():
+            messages.success(request,'no email matchs ',email)
+            return redirect ('/forgetPassword')
+        email_obj= RegisterFreelancer.get(email=email)
+        send_forget_password_mail(email_obj , token)
