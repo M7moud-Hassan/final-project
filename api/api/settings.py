043f20fbf0b531898b2ecd992a1b2d406a2cfa1c
mahmoud hassan <abosamour990@gmail.com>
2023-03-08 18:11:40 +0200

    task 1

--- api/api/settings.py
+++ api/api/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authentication'
 ]
 
 MIDDLEWARE = [
@@ -77,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'api',
         'USER':'postgres',
-        'PASSWORD':'234',
+        'PASSWORD':'mahm01142',
         'HOST':'127.0.0.1',
         'PORT':5432
     }
--- api/authentication/models.py
+++ api/authentication/models.py
@@ -4,32 +4,31 @@ from django.db import models
 
 
 class CategoryService (models.Model):
+    id = models.AutoField
     name = models.CharField(max_length=50)
-    id = models.autoField()
-
 
 class Services (models.Model):
     id = models.AutoField
-    name = models.CharField(50)
+    name = models.CharField(max_length=50)
     category_service = models.ForeignKey(CategoryService, on_delete=models.CASCADE)
 
 
 class Experience (models.Model):
-    title = models.CharField(50, required=True)
-    company = models.CharField(100, required=True)
-    location = models.CharField(200, required=False)
+    title = models.CharField(max_length=50)
+    company = models.CharField(max_length=100)
+    location = models.CharField(max_length=200)
     is_current_work_in_company = models.BooleanField(default=False)
     start_date = models.DateField()
     end_date = models.DateField()
-    description = models.CharField(500)
+    description = models.CharField(max_length=500)
     id = models.AutoField
 
 
 class Education (models.Model):
-    school = models.CharField(50)
-    degree = models.CharField(100)
-    study = models.CharField(100)
+    school = models.CharField(max_length=50)
+    degree = models.CharField(max_length=100)
+    study = models.CharField(max_length=100)
     from_year = models.IntegerField(4)
     to_year = models.IntegerField(4)
-    description = models.var(500)
+    description = models.CharField(max_length=500)
     id = models.AutoField
