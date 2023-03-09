84ee348351f0cb67d0b0bbf8d35b7fe8a9c8e432
mahmoud hassan <abosamour990@gmail.com>
2023-03-09 17:12:31 +0200

    fix debug

--- api/authentication/views.py
+++ api/authentication/views.py
@@ -7,16 +7,10 @@ from rest_framework.decorators import api_view
 
 @api_view(['GET'])
 def verfy_email(request,token,uid):
-    print(token)
-    print(uid)
-    try:
         uid = force_str(uid)
-        # user = RegisterFreelancer.objects.filter(id=uid)
-        user = get_user_model().objects.get(id=uid)
-    except (TypeError, ValueError, OverflowError, get_user_model().DoesNotExist):
-        return None
-
+        user = RegisterFreelancer.objects.filter(id=uid)
     if user is not None and email_verification_token.check_token(user, token):
-        return "OK"
-
-    return None
+        user.is_activate=True
+        return Response(user)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
