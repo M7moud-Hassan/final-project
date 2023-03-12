6312e0f86aa513f48d952f7385e761f2faf1114d
Safaa00 <hlabdalrhym@gmail.com>
2023-03-12 23:32:54 +0200

    addTask

--- api/authentication/views.py
+++ api/authentication/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+def check_email_freelancer(request) :
+    email = request.data['email']
+    user = RegisterFreelancer.objects.filter(email=email).first()
+    if user:
+        return response('ok')
+    else:
+        return not found
+
+
+def check_email_user(request) :
+    email = request.data['email']
+    user = RegisterUser.objects.filter(email=email).first()
+    if user:
+        return response('ok')
+    else:
+        return not found
