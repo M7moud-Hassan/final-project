ed060e849ea402e52a9956f9a5c66f086f6694c8
mahmoud hassan <abosamour990@gmail.com>
2023-03-16 10:47:47 +0200

    merge safaa

--- api/authentication/models.py
+++ api/authentication/models.py
@@ -69,23 +69,3 @@ class RegisterUser(models.Model):
     image=models.ImageField(upload_to='static_dirs/images/user_image')
 
 
-class Work_History(models.Model):
-    id = models.AutoField
-    id = models.ForeignKey(RegisterFreelancer, on_delete=models.CASCADE)
-    location = models.CharField(max_length=100)
-    date = models.DateField()
-    cons = models.DecimalField()
-
-class imagesProject(models.Model):
-    id = models.AutoField
-    image = models.ImageField
-
-
-class portflio(models.Model):
-    id = models.AutoField
-    id = models.ForeignKey(RegisterFreelancer, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
-    images = models.ManyToManyField(imagesProject)
-    linkVide = models.URLField()
-    description = models.CharField(max_length=500)
-
