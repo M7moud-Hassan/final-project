06cbaf0a4ffff345278aaeee934e97db799548b9
Ahmed Abdelnasser <ahmed.abdelnasser7777777@gmail.com>
2023-03-26 12:50:08 +0200

    s

--- api/api/settings.py
+++ api/api/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'api',
         'USER':'postgres',
-        'PASSWORD':'mahm01142',
+        'PASSWORD':'0965211901',
         'HOST':'127.0.0.1',
         'PORT':5432
     }
